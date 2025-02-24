b0c95fea5fa38ddfdea5c7f6a6e05916026b8465
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2025-02-24 20:46:10 -0300

    Publish local repository

---
+++ TechLibrary.Api/Infraestructure/Security/Tokens/Access/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure.Security.Tokens.Access
+{
+    public class JwtTokenGenerator
+    {
+        public string Generate(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(securityToken); 
+        }
+
+        private SymmetricSecurityKey SecurityKey()
+        {
+            // Essa key precisa ser armazenada de forma segura
+            // É ela quem vai determinar qual é a chave para criptografar as partes do token,
+            // para garantir que só a sua api que consegue gerar tokens válidos, e também validar.
+            // Para testar gerar uma password aleatório de 32 caracteres   
+            // Lembrando que essa signingKey deve ser implementada com injeção de dependência
+            // Inserir a key em Program.cs
+            var signingKey = "";
+
+            var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+
+            return new SymmetricSecurityKey(symmetricKey);
+        }
+    }
+}
---
+++ TechLibrary.Api/Program.cs
@@ -0,0 +1,89 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using TechLibrary.Api.Filters;
+
+const string AUTHENTICATION_TYPE = "Bearer";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(AUTHENTICATION_TYPE, new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = AUTHENTICATION_TYPE
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = AUTHENTICATION_TYPE
+                },
+                Scheme = "oauth2",
+                Name = AUTHENTICATION_TYPE,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKey()
+        };
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+SymmetricSecurityKey SecurityKey()
+{
+    // Gerar password aleatório de 32 caracteres   
+    // Lembrando que essa signingKey deve ser implementada com injeção de dependência
+    // // Inserir a key em JwtTokenGenerator.cs
+    var signingKey = "";
+
+    var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+
+    return new SymmetricSecurityKey(symmetricKey);
+}
